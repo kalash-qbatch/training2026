d774f43f6280d7046ea1799bc63c65aefdde904e
Kalash <kalash@qbatch.com>
2026-08-21 13:09:24 +0500

    trying to resolve workflow issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +9,7 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url:
+      process.env.DATABASE_URL || "postgresql://placeholder:placeholder@localhost:5432/placeholder",
   },
 });
